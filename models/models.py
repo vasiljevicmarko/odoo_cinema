702947be68668dafdff57e55da9be07375285e1d
vasiljevicmarko <vasiljevic.marko@outlook.com>
2019-07-19 02:06:40 +0200

    Task 3 Sell wizard added (model.view, action to create invoce to sell the tickets, invoce validatation and send email notification automacly)

--- models/models.py
+++ models/models.py
@@ -13,7 +13,7 @@
 #     @api.depends('value')
 #     def _value_pc(self):
 #         self.value2 = float(self.value) / 100
-from odoo import models, fields, api
+from odoo import models, fields, api, exceptions
 
 class movie(models.Model):
     _name = "movie"
@@ -55,6 +55,75 @@ class movie_timetable(models.Model):
             else:
                 record.remaining_seats = 0
 
+    @api.multi
+    def sell_tickets_wizard(self):
+
+        return {
+
+            'type': 'ir.actions.act_window',
+            'name': 'sell_cinema_tickets_wizard_form',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'context': {},
+            'res_model': 'sell.cinema.tickets.wizard',
+            'target': 'new',
+
+        }
+
+class sell_cinema_tickets_wizard(models.TransientModel):
+    _name = "sell.cinema.tickets.wizard"
+    _description = "Wizard for selling tickets"
+
+    def _get_movies_timetable(self):
+        return self.env['movie.timetable'].browse(self._context.get('active_ids'))
+
+    tickets = fields.Integer("Number of tickets")
+    movies_timetable = fields.Many2one('movie.timetable', string="Movies Timetable", default=_get_movies_timetable)
+    partner_id = fields.Many2one('res.partner', string="Partner", required=True, domain="[('customer', '=', True)]")
+    product_id = fields.Many2one('product.product', string="Product", required=True, domain="[('e_ticket', '=', True)]")
+
+    @api.multi
+    def sell_tickets(self):
+        for record in self:
+            if record.movies_timetable:
+                if not record.product_id.e_ticket:
+                    raise exceptions.Warning('Warning!!! Only eTicket product can be used in this transaction!')
+                if record.tickets < 1:
+                    raise exceptions.Warning('Warning!!! Number of tickets to sold must be grater than 1!')
+                elif record.movies_timetable.remaining_seats < record.tickets:
+                    raise exceptions.Warning('Warning!!! Number of remain seats is less than the number of tickets to sell!')
+                else:
+                    record.movies_timetable.sold_seats += record.tickets
+                    invoice = self.env['account.invoice'].create(
+                        {
+                            'partner_id':record.partner_id.id
+                        }
+                    )
+                    self.env['account.invoice.line'].create(
+                        {
+                            'name': record.product_id.name,
+                            'product_id': record.product_id.id,
+                            'quantity': record.tickets,
+                            'invoice_id': invoice.id,
+                            'price_unit': record.product_id.lst_price,
+                            'account_id' : invoice.account_id.id
+                        }
+                    )
+                    invoice.action_invoice_open()
+                    invoice.action_invoice_sent()
+                    return {
+
+                        'type': 'ir.actions.act_window',
+                        'name': 'account.invoice_form',
+                        'view_type': 'form',
+                        'view_mode': 'form',
+                        'context': {},
+                        'res_model': 'account.invoice',
+                        'res_id': invoice.id,
+                        'target': 'current',
+
+                    }
+
 class cinema_room(models.Model):
     _name = "cinema.room"
     _description = "Cinema Room"
@@ -62,6 +131,8 @@ class cinema_room(models.Model):
     name = fields.Char("Name", require=True)
     capacity = fields.Integer("Capacity", require=True)
     timetable = fields.One2many('movie.timetable', 'room', string='Room')
+
+
 
 class product_product(models.Model):
     _name = "product.product"
